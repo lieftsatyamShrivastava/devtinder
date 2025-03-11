bd6b00b289c936519fe6dce1f6c73c841c7c67e4
lieftsatyamsrivastava <satyamsrivastava384@gmail.com>
2025-03-11 11:53:11 +0530

    init

--- src/app.js
+++ src/app.js
@@ -2,5 +2,21 @@
 
  const app = express();
 
+ app.use("/ ", (req, res) =>{
+   res.send("Namaste Akshay!");    
 
- app.listen(3000);
+});
+
+app.use("/hello",(req,res) =>{
+    res.send("Hello hello!");
+ });
+
+ app.use("/test",(req,res) =>{
+    res.send("Hello hello!");
+ });
+
+
+ app.listen(7777, () => {
+    console.log("Server is successfully ")
+    
+ });
